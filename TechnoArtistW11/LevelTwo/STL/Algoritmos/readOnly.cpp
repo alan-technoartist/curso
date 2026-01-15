4271963e66bae073a5ea24044a2aea5916c86e95
Alan Bautista <Alan.Bautista@rinf.tech>
2026-01-14 20:26:25 -0600

    Add more algos

--- TechnoArtistW11/LevelTwo/STL/Algoritmos/readOnly.cpp
+++ TechnoArtistW11/LevelTwo/STL/Algoritmos/readOnly.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <numeric>
 #include <vector>
 #include <list>
 
@@ -25,30 +26,64 @@ void testAlgoritmosRO() {
 		{104, 100.4,	"RECHAZADA"}
 	};
 
-	//for (const auto& d : container)
+	std::cout << "Transacciones:" << std::endl;
+	for (auto t : transacciones) {
+		std::cout << t.id << " | $" << t.monto << std::endl;
+	}
 
 	// Hay montos críticos? (any_of ?)
 	bool resultado = std::any_of(transacciones.begin(), transacciones.end(),
-		[](const auto& t) {	return (t.monto > 20000); } );
+		[](const auto& t) {	return (t.monto > 20000); });
 
 	if (resultado == true)
-		std::cout << "Transaccion critica encontrada!" << std::endl;
+		std::cout << "Transaccion critica detectada!" << std::endl;
 
 	// Rechazos (count_if)
 	auto rechazos = std::count_if(transacciones.begin(), transacciones.end(),
 		[](const auto& t) { return (t.estado == "RECHAZADA"); });
 
-	std::cout << "Transacciones rechazadas: " << rechazos << std::endl;
+	std::cout << "# Transacciones rechazadas: " << rechazos << std::endl;
 
-	//resultado = std::find(transacciones.begin(), transacciones.end(), tRechazada);
+	// Detalle de rechazados (copy_if)
+	std::list<Transaccion> tRechazadas;
 
-	//std::cout << "Transaccion rechazada: #" << resultado->id << std::endl;
+	std::copy_if(transacciones.begin(), transacciones.end(), std::back_inserter(tRechazadas),
+		[](const auto& t) { return (t.estado == "RECHAZADA"); });
+
+	for (const auto& t : tRechazadas) {
+		std::cout << t.id << " | $" << t.monto << std::endl;
+	}
 
 	// Siguiente a revisar (find_if)
+	auto res = std::find_if(transacciones.begin(), transacciones.end(),
+		[](const auto& t) { return (t.estado == "EN TRANSITO"); });
+
+	if (res != transacciones.end())
+		std::cout << "Siguiente transaccion en transito: " << res->id << std::endl;
+
+
+	// lambda para acumular sumas
+	auto sumaAcumulada = [](const double& x, const Transaccion& y) {
+		return (x + y.monto);
+		};
 
 	// Total (accumulate)
+	double total = std::accumulate(transacciones.begin(), transacciones.end(), 0.0, sumaAcumulada);
+
+	std::cout << "Total: $" << total << std::endl;
 
 	// Mayor transacción (max_element)
+	auto maximoValor = std::max_element(transacciones.begin(), transacciones.end(),
+		[](const auto& a, const auto& b) { return (a.monto < b.monto); });
+
+	std::cout << "Transaccion maxima: $" << maximoValor->monto << std::endl;
 
 	// Imprimir transacciones (for_each)
+	std::for_each(transacciones.begin(), transacciones.end(),
+		[](Transaccion& t) { t.monto *= 1.16; });
+
+	std::cout << "Transacciones + IVA:" << std::endl;
+	for (auto t : transacciones) {
+		std::cout << t.id << " | $" << t.monto << std::endl;
+	}
 }
