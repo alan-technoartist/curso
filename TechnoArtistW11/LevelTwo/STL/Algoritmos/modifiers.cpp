885199fe0fca8483137cdfd9937904aa6ecc387e
Alan Bautista <Alan.Bautista@rinf.tech>
2026-01-20 18:48:25 -0600

    Add transform

--- TechnoArtistW11/LevelTwo/STL/Algoritmos/modifiers.cpp
+++ TechnoArtistW11/LevelTwo/STL/Algoritmos/modifiers.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
 
 struct Heroe {
     std::string nombre;
@@ -13,7 +14,6 @@ void imprimirEquipo(const std::vector<Heroe>& heroes) {
         std::cout << h.nombre << " | " << h.salud << " | " << h.estado << std::endl;
     }
 }
-
 
 void testModifiers() {
 
@@ -32,16 +32,19 @@ void testModifiers() {
     std::erase_if(equipo, [](Heroe& e) { return (e.estado == "FALLECIDO"); });
 
     // 2. CURA (std::for_each actuando como replace lógico)
+    //    std::for_each puede trabajar en paralelo y basado en rangos del contenedor
     std::for_each(equipo.begin(), equipo.end(),
         [](Heroe& h) {
+            // realizar operación
             if (h.estado != "SANO")
                 h.salud += 30;
+
+            if (h.salud > 100)
+                h.salud = 100;
         });
 
     std::cout << "Despues de entierros y curaciones: " << std::endl;
     imprimirEquipo(equipo);
-
-    // TODO: std::transform
 
     // 3. ORDENAR AL EQUIPO EQUIPO POR NOMBRE (std::sort)
     std::sort(equipo.begin(), equipo.end(),
@@ -54,7 +57,7 @@ void testModifiers() {
 
     Heroe lider = { "Personaje5", 0, "" };
 
-    // 4. PASAR LISTA (std::binary_search)
+    // 4. PASAR LISTA (std::binary_search) O(log n)
     bool existeLider = std::binary_search(equipo.begin(), equipo.end(), lider,
         [](const Heroe& a, const Heroe& b) {
             return (a.nombre < b.nombre);
@@ -68,11 +71,34 @@ void testModifiers() {
     }
 
     // 5. INICIATIVA (std::shuffle)
-    //std::shuffle(equipo.begin(), equipo.end(), );
+    std::random_device rd;
+    std::mt19937 motor(rd());
+    std::shuffle(equipo.begin(), equipo.end(), motor);
+
+    std::cout << "Equipo barajeado: " << std::endl;
+    imprimirEquipo(equipo);
 
     // 6. ESTRATEGIA (std::rotate/std::reverse)
     std::reverse(equipo.begin(), equipo.end());
 
-    std::cout << "Orden revertido: " << std::endl;
+    std::cout << "Orden invertido: " << std::endl;
     imprimirEquipo(equipo);
+
+    // 7. CALCULAR PUNTAJES
+    std::vector<int> puntajes_combate;
+
+    std::transform(equipo.begin(), equipo.end(), std::back_inserter(puntajes_combate),
+        [](const Heroe& h) {
+            int score = h.salud * 10;
+            if (h.estado == "HERIDO") {
+                score /= 2; // Penalización
+            }
+            return score;
+        }
+    );
+
+    std::cout << "Puntajes: " << std::endl;
+    for (int i = 0; i < equipo.size(); i++) {
+        std::cout << equipo[i].nombre << " -> Score: " << puntajes_combate[i] << std::endl;
+    }
 }
